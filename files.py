0727453ea5983f71ee5f4266c5531d91e656f8a5
Bassil Virk <bassillooney@outlook.com>
2022-02-13 13:02:14 -0500

    Commit

--- files.py
+++ files.py
@@ -3,6 +3,7 @@ fileHandler = open("text.txt", "a")
 fileHandler.write("Lots of text here\n")
 fileHandler.write("This is more text.")
 fileHandler.write("More.")
+fileHandler.write("More.")
 
 fileHandler.close()
 
